7028ba83a442994da1e30183a772a3a8664de367
kwgch <m.kwgch@gmail.com>
2015-01-18 16:36:41 +0900

    Add link to gmap infowindow

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,11 +1,11 @@
 class MapController < ApplicationController
+
   def index
     @post = Post.all
     @hash = Gmaps4rails.build_markers(@post) do |post, marker|
       marker.lat post.latitude
       marker.lng post.longitude
-      marker.infowindow post.body
-      marker.json({title: post.title})
+      marker.infowindow ActionController::Base.helpers.link_to(post.title, post_path(post))
     end
   end
 end
